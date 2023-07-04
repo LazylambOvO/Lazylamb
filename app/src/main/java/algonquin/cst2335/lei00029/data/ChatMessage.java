f6ff50bde525fccea92b81f0c4b8992ad1c9300b
Yucong Lei <lei00029@algonquinlive.com>
2023-07-04 17:49:05 -0400

    Week6 Finished Week 6

---
+++ app/src/main/java/algonquin/cst2335/lei00029/data/ChatMessage.java
@@ -0,0 +1,25 @@
+package algonquin.cst2335.lei00029.data;
+public class ChatMessage {
+
+    private final String message;
+    private final String timeSent;
+    private final boolean isSentButton;
+
+    public ChatMessage(String m, String t, boolean sent){
+        this.message = m;
+        this.timeSent = t;
+        this.isSentButton = sent;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public boolean isSentButton() {
+        return isSentButton;
+    }
+}
